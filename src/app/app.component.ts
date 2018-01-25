ac7e289810187624219635e0f9fb82747e7b00fa
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-01-25 12:08:31 -0500

    [1] Add termsAndCondition page to show on the initial load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { TermsAndConditionsPage } from '../pages/terms-and-conditions/terms-and-conditions';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = TermsAndConditionsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TermsAndConditionsPage } from '../pages/terms-and-conditions/terms-and-conditions';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TermsAndConditionsPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +21,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TermsAndConditionsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
